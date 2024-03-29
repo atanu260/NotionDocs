a91524034af4379ba08939bd45cf3a3f9cf4b5f5
atanu260 <atanuchowdhary36698@hotmail.com>
2024-03-29 20:33:17 +0530

    Add project files.

---
+++ Controllers/DocumentsController.cs
@@ -0,0 +1,159 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NotionDocs.Data;
+using NotionDocs.Models;
+
+namespace NotionDocuments.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Documents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = from c in _context.Documents
+                                       select c;
+            applicationDbContext = applicationDbContext.Where(a => a.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            return View(await applicationDbContext.Include(d => d.User).ToListAsync());
+        }
+
+
+
+        // GET: Documents/Create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,UserId")] Documents doc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(doc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(doc);
+        }
+
+        // GET: Documents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+
+            var doc = await _context.Documents.FindAsync(id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+            if (doc.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(doc);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,UserId")] Documents doc)
+        {
+            if (id != doc.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(doc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocExists(doc.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", doc.UserId);
+            return View(doc);
+        }
+
+        // GET: Documents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Documents == null)
+            {
+                return NotFound();
+            }
+
+            var doc = await _context.Documents
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+            if (doc.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(doc);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Documents == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Documents'  is null.");
+            }
+            var doc = await _context.Documents.FindAsync(id);
+            if (doc != null)
+            {
+                _context.Documents.Remove(doc);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocExists(int id)
+        {
+            return (_context.Documents?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotionDocs.Models;
+using System.Reflection.Metadata;
+
+namespace NotionDocs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Documents> Documents { get; set; }
+    }
+}
